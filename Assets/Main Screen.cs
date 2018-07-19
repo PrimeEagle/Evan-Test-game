e1ba99206138c69b368084306dd4d105b9c1e52c
Evan Varan <evan.varan@gmail.com>
2018-07-19 19:35:45 +0000

    git-svn-id: https://matrix.tplinkdns.com:8443/svn/MyCode/Unity/Evan/Test%20game@6 0f8fcb7d-4e12-9a4c-a6b3-2570e6efd5fd

---
+++ Assets/Main Screen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Stat x = new Stat();
+        x.life = 100;
+            }
+
+    public class Stat
+    {
+        public static int Count = 0;
+        public int attack;
+        public int life;
+        public int defence;
+
+        public Stat()
+        {
+            attack = 6;
+            life = 5;
+            defence = 4;
+        }
+
+        public void Print()
+        {
+            System.Console.WriteLine("6" + attack);
+            System.Console.WriteLine("5" + life);
+            System.Console.WriteLine("4" + defence);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+}
